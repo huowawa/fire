46a1b3cdf041f6de0e9fa86c5e6e8cd93c227b83
huowawa <1608692339@qq.com>
2020-03-29 17:46:02 +0800

    修改了全局响应策略

--- src/main/java/com/soft/fire/common/GlobalResponseHandler.java
+++ src/main/java/com/soft/fire/common/GlobalResponseHandler.java
@@ -3,6 +3,7 @@
  */
 package com.soft.fire.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -19,14 +20,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  * @date 2020-03-28 18:00
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
 
     @Override
     public boolean supports(MethodParameter methodParameter,
                             Class<? extends HttpMessageConverter<?>> aClass) {
 
+        log.info("返回的类型为：{}", methodParameter.getGenericParameterType().getTypeName());
         // 如果接口返回的类型本身就是Result那就没有必要进行额外的操作，返回false
-        return !methodParameter.getGenericParameterType().equals(Result.class);
+        return !methodParameter.getGenericParameterType().getTypeName()
+                .equals("com.soft.fire.common.Result<java.lang.String>");
     }
 
     @Override
